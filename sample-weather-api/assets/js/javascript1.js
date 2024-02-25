91b15692131f0ba70139b96b64d78c7a79172524
GrandNagusZek <iliyas.mallard@yahoo.com>
2024-02-24 19:02:15 -0500

    started 5 day forecast function with url and api set up

--- sample-weather-api/assets/js/javascript1.js
+++ sample-weather-api/assets/js/javascript1.js
@@ -2,5 +2,27 @@ var apiKey="78e753a08d650befa72a539b438a53bf"
 
 
 function currentWeather(cityName){
-    var url=`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid={API key}`
+    var url=`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`
+
+    fetch(url)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        console.log(data)
+    })
 }
+function fiveForecast(cityName){
+    var url=`http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=imperial`
+
+    fetch(url)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        console.log(data)
+    })
+}
+
+currentWeather('Miami')
+fiveForecast('Miami')
